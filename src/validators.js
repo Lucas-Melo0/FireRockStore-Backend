80083a19996b25bd3a36f60df0340e4203b3f5fc
Lucas-Melo0 <lucassss8v@gmail.com>
2022-09-15 13:47:04 -0300

    Add signup and signin validations

---
+++ src/validators.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+const validator = (schema) => (payload) => schema.validate(payload);
+
+const signUpSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+  confirmation: Joi.ref("password"),
+});
+
+const signInSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
