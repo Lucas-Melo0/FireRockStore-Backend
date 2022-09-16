ebf9312d07ea09c6ea0ed806b3b3f5825228ac80
sanditomaz <sandi.contato@gmail.com>
2022-09-16 16:10:19 -0300

    Added dotEnv package

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
 try {
   await mongoClient.connect();
 } catch (err) {
