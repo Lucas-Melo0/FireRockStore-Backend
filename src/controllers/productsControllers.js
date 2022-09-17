e47d2ca5c5fc603fe609f67732bd0639b377bc9c
sanditomaz <sandi.contato@gmail.com>
2022-09-17 17:45:31 -0300

    fixed db routes

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -2,11 +2,10 @@ import { db } from "../database/db.js";
 
 async function listHomePageProducts(req, res) {
   try {
-    const teste = await db.collection("products").find().toArray();
-    const sera = teste.map((i) => i.products);
-    console.log(sera);
+    const teste = await db.collection("users").find().toArray();
+    console.log(teste);
 
-    res.sendStatus(200);
+    res.status(200).send("shhhhh");
   } catch (error) {
     res.sendStatus(500);
   }
@@ -14,8 +13,10 @@ async function listHomePageProducts(req, res) {
 
 async function productInsertion(req, res) {
   try {
-    await db.collection("products").insertOne(req.body);
-    return res.sendStatus(200);
+    const vision = db.collection("teste3").insertMany(req.body);
+
+    console.log(vision);
+    return res.status(200).send("hiiiii");
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
